738090f7651e3055a9230e2bd71ca5b52771ee31
Saleem_Abou_Assi <amhadi696@gmail.com>
2024-07-28 12:01:37 +0300

    order api controller

--- app/Http/Controllers/Api/ApiOrderController.php
+++ app/Http/Controllers/Api/ApiOrderController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Cart;
+use App\Models\Order;
+use App\Models\OrderProduct;
+use App\Models\Product;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class ApiOrderController extends Controller
@@ -11,6 +16,46 @@ class ApiOrderController extends Controller
     {
         $orders = Order::with('product')->get();
 
-        return view('order.index',['orders'=>$orders]);
+        return response()->json(['orders'=>$orders]);
+
+    }
+
+    public function add($user_id)
+    {
+        $carts = Cart::where('user_id', $user_id)->get();
+        
+        
+        $order = new Order;
+        $order->user_id = $user_id;
+
+        $total_price = 0;
+        foreach($carts as $cart)
+        {
+            $product = Product::where('id',$cart->product_id)->first();
+            $amount = $cart->amount;
+            $total_price += $product->price * $amount;
+        
+        }
+
+        $order->total_price = $total_price;
+        $order->created_at = Carbon::now();
+        $order->save();
+
+        foreach($carts as $cart)
+        {
+            $order_product = new OrderProduct();
+            $order_product->order_id = $order->id;
+            $order_product->product_id = $cart->product_id;
+            $order_product->amount = $cart->amount;
+            $order_product->save();
+            $cart->delete();
+        }
+
+        
+
+        
+        return response()->json(['message'=>"order added successfully"]);
+        
+        
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\ApiCartController;
+use App\Http\Controllers\Api\ApiOrderController;
 use App\Http\Controllers\Api\ApiUserDetailsController;
 use App\Http\Controllers\Api\ApiProductController;
 use Illuminate\Support\Facades\Route;
@@ -23,4 +24,7 @@ use Illuminate\Support\Facades\Route;
     Route::delete('/cart/delete/{cart_id}',[ApiCartController::class, 'destroy']);
     Route::post('/cart/add',[ApiCartController::class, 'add']);
 
+    //order
+    Route::get('/order/index',[ApiOrderController::class, 'index']);
+    Route::get('/order/add/{user_id}',[ApiOrderController::class, 'add']);
 
